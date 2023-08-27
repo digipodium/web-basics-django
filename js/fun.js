4fabe66d89de5323812eb09939a377f4e586e834
zaid kamil <xaidmetamorphos@gmail.com>
2023-08-27 20:09:00 +0530

    js 2

---
+++ js/fun.js
@@ -0,0 +1,17 @@
+// create a function using classic syntax
+function showAlert(){
+    alert("I am from classic function");
+}
+// function stored in a variable
+f = function(){
+    console.log("I am from function stored in a variable");
+}
+// es6 arrow function
+g = () => {
+    console.log("I am from arrow function");
+}
+// classic func with parameters
+function add(a,b){
+    return a+b;
+}
+
